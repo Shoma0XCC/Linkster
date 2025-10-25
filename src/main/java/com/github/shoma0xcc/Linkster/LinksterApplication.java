7b8604af46c440e7ea5d296a93ca823678ab030f
shamil <shamilnigmatzynov@ya.ru>
2025-10-25 16:44:06 +0500

    Init

---
+++ src/main/java/com/github/shoma0xcc/Linkster/LinksterApplication.java
@@ -0,0 +1,13 @@
+package com.github.shoma0xcc.Linkster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinksterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/shoma0xcc/Linkster/LinksterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.shoma0xcc.Linkster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
